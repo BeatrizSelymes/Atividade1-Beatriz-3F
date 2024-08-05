75a2b09f48467ca4602d952f3086305db278309b
BeatrizSelymes <164294239+BeatrizSelymes@users.noreply.github.com>
2024-08-05 22:55:51 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const meuNome = "Beatriz Selymes";
+const idade = 17;
+const altura = 1.63;
+const massa = 53;
+const cidade = "Itanhaém-SP";
+
+console.log(meuNome);
+console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidade);
